1577a22017e178c955860ab81be5dbaabfdc2a14
mambamentality8 <858968850@qq.com>
2019-04-09 23:17:55 +0800

    完整版springcloud整合各大组件

---
+++ demo18_docker-demo/src/main/java/com/example/dockerdemo/DockerDemoApplication.java
@@ -0,0 +1,28 @@
+package com.example.dockerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootApplication
+@Controller
+public class DockerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerDemoApplication.class, args);
+    }
+
+    @RequestMapping("/user/find")
+    @ResponseBody
+    public Object findUser(){
+        Map<String,String> map = new HashMap<>();
+        map.put("name","xiaoming");
+        map.put("age","28");
+        return map;
+    }
+}
